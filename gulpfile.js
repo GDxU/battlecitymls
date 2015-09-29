e195f1659a863047e26e17127dfed842bc3c1760
frank fang <1179750402@qq.com>
2015-09-29 11:23:49 +0800

    用typescript重构

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var merge = require('merge2');
+
+var tsProject = ts.createProject({
+    declaration: true,
+    noExternalResolve: true
+});
+var tspath = 'tsjs/*.ts';
+gulp.task('buildts', function () {
+    var tsResult = gulp.src(tspath)
+                    .pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest('js'));
+});
+gulp.task('watch', ['buildts'], function () {
+    gulp.watch(tspath, ['buildts']);
+});
