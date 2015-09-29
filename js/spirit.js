067f3606500943851df79fae8a09e30fe8770eaa
frank fang <1179750402@qq.com>
2015-09-29 13:49:09 +0800

    add tank

--- js/spirit.js
+++ js/spirit.js
@@ -1,8 +1,18 @@
 var game;
 (function (game) {
     var spirit = (function () {
-        function spirit() {
+        function spirit(point) {
+            this.point = point;
+            this.id = common.newId();
         }
+        Object.defineProperty(spirit.prototype, "imgSrc", {
+            set: function (src) {
+                this.image = new Image();
+                this.image.src = src;
+            },
+            enumerable: true,
+            configurable: true
+        });
         return spirit;
     })();
     game.spirit = spirit;
