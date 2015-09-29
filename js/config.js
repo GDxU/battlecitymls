994694d8702663e016f731b85a097567067fb362
frank fang <1179750402@qq.com>
2015-09-29 12:38:51 +0800

    基础

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var game;
+(function (game) {
+    game.config = {
+        maxWidth: 600,
+        maxHeight: 600
+    };
+})(game || (game = {}));
---
+++ tsjs/spirit.ts
@@ -0,0 +1,5 @@
+﻿module game {
+    export class spirit {
+
+    }
+}
