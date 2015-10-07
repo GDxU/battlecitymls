41c4c3112ebf94c36317e583a3acdc5f45d4e92f
lred <1179750402@qq.com>
2015-10-07 11:50:34 +0800

    add terrain.js

---
+++ js/terrain.js
@@ -0,0 +1,19 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var game;
+(function (game) {
+    var terrain = (function (_super) {
+        __extends(terrain, _super);
+        function terrain() {
+            _super.apply(this, arguments);
+        }
+        terrain.prototype.draw = function (canvas) {
+            _super.prototype.draw.call(this, canvas);
+        };
+        return terrain;
+    })(game.spirit);
+    game.terrain = terrain;
+})(game || (game = {}));
